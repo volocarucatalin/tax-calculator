fcf927673de5be99cb278a966e21512d1de82175
sergiu221 <sergiu221@gmail.com>
2025-01-30 16:12:59 +0200

    Add security

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
-CREAT TABLE contractor (
+CREAT
+TABLE contractor (
   id int NOT NULL AUTO_INCREMENT,
   address varchar(255) DEFAULT NULL,
   email varchar(255) DEFAULT NULL,
@@ -20,3 +21,27 @@ CREATE TABLE sub_contractor
     CONSTRAINT FKhgp34640pff2wfwv1qa5yj94w FOREIGN KEY (sub_contractor_list_id) REFERENCES contractor (id),
     CONSTRAINT FKqhp0ayg565tbguobj62jsphsn FOREIGN KEY (contractor_id) REFERENCES contractor (id)
 );
+
+
+CREATE TABLE users
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Primary key
+    username VARCHAR(255) NOT NULL,             -- Username field
+    email    VARCHAR(255) NOT NULL,             -- Email field
+    password VARCHAR(255) NOT NULL,             -- Password field
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    active   BOOLEAN      NOT NULL              -- Active status (true/false)
+);
+
+ALTER TABLE Users AUTO_INCREMENT = 1;
+
+CREATE TABLE users_seq (
+                           id BIGINT NOT NULL AUTO_INCREMENT,
+                           PRIMARY KEY (id)
+);
+
+commit;
+
+
+drop table  users_seq;
---
+++ src/main/java/com/repositoris/UserRepository.java
@@ -0,0 +1,11 @@
+package com.repositoris;
+
+import com.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+}
