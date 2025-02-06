81f8699a8b22595a2e49660eff0eabd0bcf4efb1
sergiu221 <sergiu221@gmail.com>
2025-02-06 18:06:01 +0200

    Try to fix cross origin

--- src/main/java/com/TaxCalculatorApplication.java
+++ src/main/java/com/TaxCalculatorApplication.java
@@ -3,36 +3,20 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 public class TaxCalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaxCalculatorApplication.class, args);
-	}
-
-	//Here is main , were the application is stating.
-	@Bean
-	public CorsFilter corsFilter(){
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin" , "Access-Control-Allow-Origin"
-				, "Content-Type", "Accept","Authorization",
-				"Origin", "Accept","X-Request-With",
-				"Access-Control-Request-Method",
-				"Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type",
-				"Accept","Authorization",
-				"Access-Control-Allow-Origin" ,"Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**" , corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaxCalculatorApplication.class, args);
+    }
 }
--- src/main/java/com/config/ApplicationConfig.java
+++ src/main/java/com/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.repositoris.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -40,5 +44,24 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //Here is main , were the application is stating.
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.OPTIONS.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION);
+            }
+        };
     }
 }
