7e3ddc337e741bbe50413b3aa2d7410f32722d16
volocarucatalin <volocaru.catalin17@gmail.com>
2025-02-08 20:36:20 +0000

    Update Invoice

--- src/main/java/com/controllers/InvoiceController.java
+++ src/main/java/com/controllers/InvoiceController.java
@@ -1,6 +1,10 @@
 package com.controllers;
 
+import com.request.InvoiceRequest;
+import com.services.InvoiceService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/invoices")
 public class InvoiceController {
 
+    private final InvoiceService invoiceService;
 
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @PostMapping("/register")
+    public void registerInvoice(@RequestBody InvoiceRequest invoiceRequest) {
+          invoiceService.createInvoice(invoiceRequest);
+    }
 
 }
